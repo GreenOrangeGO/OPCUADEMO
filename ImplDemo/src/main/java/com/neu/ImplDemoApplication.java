f2bee73565252b31785ecd0590fd801361550449
Green_Orange_GO <1013986543@qq.com>
2024-04-26 23:16:55 +0800

    初始代码

---
+++ ImplDemo/src/main/java/com/neu/ImplDemoApplication.java
@@ -0,0 +1,13 @@
+package com.neu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplDemoApplication.class, args);
+    }
+
+}
